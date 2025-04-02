ccd0943b5ade53971dad81a9e6bb0a4c7681b64a
TonyMarkham <myonlineentity@gmail.com>
2025-04-02 19:35:54 -0400

    update

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -11,26 +11,48 @@ def generate_launch_description():
     # Force sim time to be enabled
     package_name='articubot-one' # <-- CHANGE ME
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
+      PythonLaunchDescriptionSource([os.path.join(
+        get_package_share_directory(package_name),'launch','rsp.launch.py')]),
+      launch_arguments={'use_sim_time': 'true'}.items())
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo_params_file = os.path.join(
+      get_package_share_directory(package_name),
+      'config',
+      'gazebo_params.yaml')
+    gazebo_params_file=os.path.join(
+      get_package_share_directory(package_name),
+      'config',
+      'gazebo_params.yaml')
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+      PythonLaunchDescriptionSource([os.path.join(
+        get_package_share_directory('gazebo_ros'),
+        'launch',
+        'gazebo.launch.py')]),
+      launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items())
 
     # Run the spawner node from the gazebo_ros package.
     # The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    spawn_entity = Node(
+      package='gazebo_ros',
+      executable='spawn_entity.py',
+      arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+      output='screen')
+    
+    diff_drive_spawner = Node(
+      package='controller_manager',
+      executable='spawner.py',
+      arguments=["diff_cont"])
+    
+    joint_broad_spawner = Node(
+      package='controller_manager',
+      executable='spawner.py',
+      arguments=["joint_broad"])
 
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
